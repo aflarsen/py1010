0a9d4c2abb0faa4a0db49c0d30d4b933f5d7fcf3
Aleksander Larsen <abflarsen@gmail.com>
2025-01-20 10:02:41 +0100

    Oppg kap 5 nesten ferdig + oppg kap 4

---
+++ Oppgaver/Oppgaver_kap5_funksjoner.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 18 17:17:26 2025
+
+@author: Aleksander Larsen
+
+Oppg. kap. 5
+
+"""
+#%% Oppg 5.1
+
+# Funksjonen ser ut til å regne ut arealet til en rettvinklet trekant. 
+
+#%% Oppg 5.2
+
+def fun_rektangel(g, h):
+    A = g*h
+    return A
+
+print('Areal =', fun_rektangel(4.5, 4))
+
+#%% Oppg 5.3
+
+def fun_valuta(e):
+    NOK = e*10.42
+    USD = e*1.19
+    return [NOK, USD]
+
+euro = float(input('Skriv inn antall Euro: '))
+
+print('€ in NOK:', round(fun_valuta(euro)[0],2), '\n€ in USD:', round(fun_valuta(euro)[1], 2))
+
+# Løsning iht boken, samme men litt annerledes:
+    
+def fun_valuta(e):
+    NOK = e*10.42
+    USD = e*1.19
+    return NOK, USD
+
+NOK, USD = fun_valuta(euro)
+
+print('€ in NOK:', NOK, '\n€ in USD:', USD)
+
+#%% Oppg 5.4
+
+import numpy as np
+
+def fun_storekatet(lk, hy):
+    sk =  np.sqrt(hy**2-lk**2)
+    # sk2 = sk = hy**2-lk**2   -uten bruk av numpy eller matlab
+    # sk = sk2**0.5
+    return sk
+
+a = float(input('Skriv inn kateten: '))
+c = float(input('Skriv inn hypotenusen: '))
+
+print(np.round(fun_storekatet(a, c), 2)) # OBS: hypotenusen må være større enn kateten, ellers blir tallet negativt som gir feil. Evt må man ha en sjekk på hvilket input-tall som er størst feks
+
+#%% Oppg 5.5
+
+# Fungerer denne, hva skrives i så fall ut?
+
+x = 10
+
+def myfun ():
+    print ( x )
+
+myfun ()
+
+# Mitt svar: Ja, det fungerer. Skriver ut 10
+
+#%% Oppg 5.6
+
+# Fungerer denne? Hva skrives evt ut?
+
+x = 10
+
+def myfun ():
+    x = x + 1
+    print ( x )
+
+myfun ()
+
+# Mitt svar: Ja, det fungerer. Skriver ut 11
+# Løsning: feil! UnboundLocalError: cannot access local variable 'x' where it is not associated with a value
+
+#%% Oppg 5.9
+
+Ep = 34.37
+m = 0.48
+g = 1.6
+
+def fun_hoydeOverBakken(Ep, m, g=9.81):
+    h = Ep/(m*g)
+    return h
+
+print(fun_hoydeOverBakken(Ep, m, g))
+
+#%% Oppg 5.10
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def fun_f(x):
+    return x**2+3
+
+def fun_g(x):
+    return 3*x-1
+
+xa = np.linspace(-2, 3, 100)
+
+plt.close('all')
+plt.figure(1, figsize=(12, 9))
+plt.plot(xa, fun_f(xa), xa, fun_g(xa))
+plt.legend(labels=('fun_f(x)', 'fun_g(x)'))
+# plt.savefig('plot_fun_plot.pdf')
+plt.show()
+
+#%% Oppg 5.16
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
